fa013454f1e677ae3fc73c73851747da866cdabd
Ignacio Eleazar Marceca <imarceca@gmail.com>
2025-04-14 01:41:58 +0200

    MM - 1 test: homepage navbar

--- tests/helpers/navigation.py
+++ tests/helpers/navigation.py
@@ -10,8 +10,66 @@ def go_to_homepage(page: Page):
         )
 
 def click_references_link(page: Page):
+    print(
+        "And clicks on References"
+        )
     page.get_by_role(
         "link", 
         name="References", 
         exact=True
         ).click()
+
+def click_learn_link(page: Page):
+    print(
+        "And clicks on Learn"
+        )
+    page.get_by_role(
+        "link", 
+        name="Learn", 
+        exact=True
+        ).click()
+    
+def click_plus_link(page: Page):
+    print(
+        "And clicks on Plus"
+        )
+    page.get_by_role(
+        "link", 
+        name="Plus", 
+        exact=True
+        ).click()
+    
+#fix all selectors below
+def click_curriculum_link(page: Page):
+    print(
+        "And clicks on Curriculum"
+        )
+    page.get_by_role(
+        "link", 
+        name="Curriculum", 
+        exact=True
+        ).click()
+    
+def click_blog_link(page: Page):
+    print(
+        "And clicks on Blog"
+        )
+    page.get_by_role(
+        "link", 
+        name="Blog", 
+        exact=True
+        ).click()
+    
+def click_tools_link(page: Page):
+    print(
+        "And clicks on Tools"
+        )
+    page.get_by_role(
+        "link", 
+        name="Tools", 
+        exact=True
+        ).click()
+    
+# idea for clicking every item in each section
+#     for li in page.get_by_role('listitem').all():
+#   li.click();
--- tests/test_homepage_navigation_bar.py
+++ tests/test_homepage_navigation_bar.py
@@ -1,9 +1,14 @@
 from playwright.sync_api import Page, expect
-from helpers.navigation import go_to_homepage, click_references_link
+from helpers.navigation import go_to_homepage, click_references_link, click_blog_link, click_curriculum_link, click_learn_link, click_plus_link, click_tools_link
 
 def test_homepage_navbar(page: Page):
     go_to_homepage(page)
     click_references_link(page)
+    click_learn_link(page)
+    click_plus_link(page)
+    click_curriculum_link(page)
+    click_blog_link(page)
+    click_tools_link(page)
     
 
     
